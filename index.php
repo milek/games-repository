cd6a3ae426d9a4ae4326df07d6c46febb4381610
Lukasz Milewski <lukasz.milewski@gmail.com>
2012-10-11 17:24:31 +0200

    Added ability to save settings

--- index.php
+++ index.php
@@ -9,6 +9,12 @@
     $categories = array();
     $games = array();
 
+    // default values
+    $filters['rented'] = $_COOKIE['selected_rented'] != null ? $_COOKIE['selected_rented'] == 'true' : true;
+    $filters['notplayed'] = $_COOKIE['selected_notplayed'] != null ? $_COOKIE['selected_notplayed'] == 'true' : true;
+    $filters['notowned'] = $_COOKIE['selected_notowned'] != null ? $_COOKIE['selected_notowned'] == 'true' : false;
+    $filters['notreleased'] = $_COOKIE['selected_notreleased'] != null ? $_COOKIE['selected_notreleased'] == 'true' : false;
+
     // load categories
     loadCategories($categories);
 
@@ -27,6 +33,7 @@
   <meta name="author" content="lukasz.milewski@gmail.com" />
   <link rel="stylesheet" type="text/css" media="screen" href="styles.css" />
   <script src="jquery.js"></script>
+  <script src="jquery.cookie.js"></script>
   <script src="jquery.tinysort.js"></script>
 </head>
 <body>
@@ -36,10 +43,10 @@
   </div>
 
   <tabs>
-    <filter id="notreleased">not released</filter>
-    <filter id="notowned">not owned</filter>
-    <filter class="selected" id="notplayed">not played</filter>
-    <filter class="selected" id="rented">rented</filter>
+    <filter id="notreleased"<?= $filters['notreleased'] ? ' class="selected"' : ""; ?>>not released</filter>
+    <filter id="notowned"<?= $filters['notowned'] ? ' class="selected"' : ""; ?>>not owned</filter>
+    <filter id="notplayed"<?= $filters['notplayed'] ? ' class="selected"' : ""; ?>>not played</filter>
+    <filter id="rented"<?= $filters['rented'] ? ' class="selected"' : ""; ?>>rented</filter>
 <?php
 
     $selectedCategory = false;
@@ -59,11 +66,21 @@
 
 <?php
 
+    $group_index = 0;
+    $currentTab = $categories[0]->id;
+
     foreach ($games as $group)
     {
+        $group_index++;
         $i = 1;
 
-?>    <ul id="content<?= $group->name ?>" class="content<?= $group->name == $categories[0]->id ? " active" : "" ?>">
+        $active = $categories[$group_index - 1]->active;
+        if ($active)
+        {
+            $currentTab = $group->name;
+        }
+
+?>    <ul id="content<?= $group->name ?>" class="content<?= $categories[$group_index - 1]->active ? " active" : "" ?>">
 <?php
 
         foreach ($group->games as $game)
@@ -73,18 +90,34 @@
             if ($game->renter != null)
             {
                 $addtnl = " rented";
+                if (!$filters['rented'])
+                {
+                    $addtnl .= " invisible";
+                }
             }
             else if ($game->released != null && $game->released != "0")
             {
-                $addtnl = " notreleased invisible";
+                $addtnl = " notreleased";
+                if (!$filters['notreleased'])
+                {
+                    $addtnl .= " invisible";
+                }
             }
             else if ($game->released != null && $game->released == "0")
             {
-                $addtnl = " notowned invisible";
+                $addtnl = " notowned";
+                if (!$filters['notowned'])
+                {
+                    $addtnl .= " invisible";
+                }
             }
             else if ($game->achPoints->value == null || $game->achPoints->value == 0)
             {
                 $addtnl = " notplayed";
+                if (!$filters['notplayed'])
+                {
+                    $addtnl .= " invisible";
+                }
             }
             else if ($game->achPoints->value == $game->achPoints->outOf)
             {
@@ -163,9 +196,10 @@
 <?php } ?>
   <div style="clear: both;"></div></div>
 
-  <div class="footer">
-    <div style="float: right"><a href="https://github.com/milek/GamesRepository">Fork it on <strong>GitHub!</strong></a> or <a href="https://bitbucket.org/verdigo/gamesrepository">on <strong>Bitbucket!</strong></a></div>
+  <footer>
+    <right><a href="https://github.com/milek/GamesRepository">Fork it on <strong>GitHub!</strong></a> or <a href="https://bitbucket.org/verdigo/gamesrepository">on <strong>Bitbucket!</strong></a></right>
     <strong>Last modified:</strong> <b><?= date("F d Y H:i:s", filemtime("data/achievements.json")) ?></b><br />
+    <right><clear>Clear my <strong>Settings</strong></clear></right>
     <strong>Sort tiles:</strong>
     <sort class="selected" id="sOaz">original a-z</sort> |
     <sort id="sOza">original z-a</sort> | 
@@ -173,13 +207,13 @@
     <sort id="sNza">name z-a</sort> |
     <sort id="sPaz">completion 0%-100%</sort> |
     <sort id="sPza">completion 100%-0%</sort>
-  </div>
+  </footer>
 
   <script>
 
-    var currentTab = '<?= $categories[0]->id ?>';
+    var currentTab = '<?= $currentTab ?>';
 
-    function changeClass(tab)
+    function changeTab(tab)
     {
         $('#content' + currentTab).removeClass('active');
         $('#' + currentTab).removeClass('active');
@@ -188,6 +222,8 @@
         $('#' + tab).addClass('active');
 
         currentTab = tab;
+
+        $.cookie('tab', tab, {expire: 365});
     }
 
     function showHide(type)
@@ -201,6 +237,7 @@
         });
 
         $('#' + type).toggleClass('selected');
+        $.cookie('selected.' + type, $('#' + type).hasClass('selected'), {expire: 365});
     }
 
     function sort(id,where,desc)
@@ -213,8 +250,18 @@
         {
             $('ul#[id^="content"]>tile').tsort(where);
         }
+
         $('sort').removeClass('selected');
         $(id).addClass('selected');
+    }
+
+    function removeSettings()
+    {
+        $.removeCookie('tab');
+        $.removeCookie('selected.rented');
+        $.removeCookie('selected.notplayed');
+        $.removeCookie('selected.notowned');
+        $.removeCookie('selected.notreleased');
     }
 
 <?php
@@ -223,7 +270,7 @@
     {
 
 ?>
-    $('#<?= $category->id ?>').click(function(){changeClass('<?= $category->id ?>')});
+    $('#<?= $category->id ?>').click(function(){changeTab('<?= $category->id ?>')});
 <?php
 
     }
@@ -243,6 +290,8 @@
     $('#sNza').click(function(){sort('#sNza','name','desc')});
     $('#sPaz').click(function(){sort('#sPaz','name');sort('#sPaz','bar')});
     $('#sPza').click(function(){sort('#sPza','name');sort('#sPza','bar','desc')});
+
+    $('clear').click(function(){removeSettings()});
 
   </script>
 
